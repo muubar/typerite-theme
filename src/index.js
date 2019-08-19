e6fd5d882d02b77602fc8900a20f5471208465d7
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2019-08-19 06:21:20 +0200

    implement non-responsive header components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,88 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style.scss";
+import logoIcon from './assets/logo.svg';
 
-class Welcome extends React.Component {
+
+function Home() {
+  return (
+    <Header currentPage="Home" />
+  )
+}
+
+class Header extends React.Component {
+  handleClick = (e) => {
+    const header = document.getElementsByClassName("header")[0];
+    const navMenu = document.getElementsByClassName("header__nav")[0];
+
+    e.currentTarget.classList.toggle("is-active");
+    header.classList.toggle("header--visible-nav");
+    navMenu.classList.toggle("header__nav--visible");
+  }
+
   render() {
     return (
-      <h1 className='orange-boy'><span>Hello World homies whats poooopin</span></h1>
+      <header className="header">
+        <a href="index.html" className="Header__logo-container">
+          <img src={logoIcon} alt="typerite's logo" className="header__logo" />
+        </a>
+        <Hamburger onClick={this.handleClick} />
+        <NavMenu currentPage={this.props.currentPage} />
+      </header>
     );
   }
 }
 
-ReactDOM.render(<Welcome />, document.getElementById("root"));
+function Hamburger(props) {
+  return (
+    <button className="hamburger hamburger--spin" type="button" onClick={props.onClick}>
+      <span className="hamburger-box">
+        <span className="hamburger-inner"></span>
+      </span>
+    </button>
+
+  )
+}
+
+class NavMenu extends React.Component {
+  handleClick = (e) => {
+    const submenu = e.target.getElementsByClassName('header__submenu')[0];
+
+    e.target.classList.toggle("has-children--open");
+    submenu.classList.toggle("header__submenu--visible");
+  }
+
+
+  render() {
+    const links = ["Home", "Posts", "About", "Contact"].map((val) => {
+      return val === this.props.currentPage ? <li key={val} className="header__nav--current-page"><a href={`${val === "Home" ? "index" : val}.html`}>{val}</a></li> : <li key={val}><a href={`${val === "Home" ? "index" : val}.html`}>{val}</a></li>
+    });
+
+    return (
+      <nav className="header__nav">
+        <ul className="header__nav-list">
+          {
+            links.map((elem) => {
+              if (elem.key === "Posts") {
+                return (
+                  <li key={elem.key} className={`${elem.props.className ? elem.props.className : ''} has-children`} onClick={this.handleClick}>
+                    Posts
+                <ul className="header__submenu">
+                      <li key="standard post"><a href="standard-post.html">standard post</a></li>
+                      <li key="video gallery"><a href="video-post.html">video gallery</a></li>
+                      <li key="audio post"><a href="audio-post.html">audio post</a></li>
+                      <li key="gallery post"><a href="gallery-post.html">gallery post</a></li>
+                    </ul>
+                  </li>
+                )
+              }
+              else return elem;
+            })
+          }
+        </ul>
+      </nav>
+    )
+  }
+}
+
+ReactDOM.render(<Home />, document.getElementById("root"));
