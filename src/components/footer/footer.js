0f3a9236b97bc23f93cd9e8fe75f34254a941085
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2019-09-08 23:22:42 +0200

    remove p element to directly manage the span elements as flex items

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,7 +5,7 @@ import "./footer.scss";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p><span>© Copyright Typerite 2019</span>|<span>Design by <a href="https://www.styleshout.com/">StyleShout</a></span></p>
+      <span>© Copyright Typerite 2019</span>|<span>Design by <a href="https://www.styleshout.com/">StyleShout</a></span>
     </footer>
   )
 }
