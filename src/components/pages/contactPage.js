c7f280b30324a0ce0f2d72c01f5aadc4c5c541c7
Ahmed <ahmedmudhar@hotmail.com>
2019-09-09 05:45:18 +0200

    refactor aboutPage and contactPage sass into a single base sass file

--- src/components/pages/contactPage.js
+++ src/components/pages/contactPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./contactPage.scss";
+import "./pages-base.scss";
 
 import Header from "../header/header";
 import Footer from "../footer/footer";
@@ -11,54 +11,56 @@ import highImg from "../../assets/contact/contact-2000.jpg";
 export default function contactPage() {
   return (
     <>
-      <Header />
-      <main>
-        <img srcSet={`${highImg} 2000w, ${medImg} 1000w, ${lowImg} 500w`} sizes="(max-width: 2000px) 100vw, 2000px"
-          src={medImg} alt="mail box" />
-        <h1>Get In Touch.</h1>
-        <p className="title_p">Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco magna amet magna cupidatat qui labore cillum sit in tempor veniam consequat non laborum adipisicing aliqua ea nisi sint.</p>
+      <div className="post--white-background">
+        <Header />
+        <main className="post-container">
+          <img className="post-header__img" srcSet={`${highImg} 2000w, ${medImg} 1000w, ${lowImg} 500w`} sizes="(max-width: 2000px) 100vw, 2000px"
+            src={medImg} alt="mail box" />
+          <h1 className="post-header__title">Get In Touch.</h1>
+          <p className="post-header__p">Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco magna amet magna cupidatat qui labore cillum sit in tempor veniam consequat non laborum adipisicing aliqua ea nisi sint.</p>
 
-        <p>Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco magna amet magna cupidatat qui labore cillum sit in tempor veniam consequat non laborum adipisicing aliqua ea nisi sint</p>
-        <div className="msgs-container">
-          <div className="msg">
-            <h4>Where to Find Us</h4>
-            <address>
-              1600 Amphitheatre Parkway <br />
-              Mountain View, CA <br />
-              94043 US
+          <p className="post-p">Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco magna amet magna cupidatat qui labore cillum sit in tempor veniam consequat non laborum adipisicing aliqua ea nisi sint</p>
+          <div className="msgs-container">
+            <div className="msg">
+              <h4 className="post-h4">Where to Find Us</h4>
+              <address className="post-address">
+                1600 Amphitheatre Parkway <br />
+                Mountain View, CA <br />
+                94043 US
           </address>
+            </div>
+            <div className="msg">
+              <h4 className="post-h4">Contact Info</h4>
+              <address className="post-address">sayhello@typerite.com <br />
+                info@typerite.com <br />
+                Phone: +197 543 2345 </address>
+            </div>
           </div>
-          <div className="msg">
-            <h4>Contact Info</h4>
-            <address>sayhello@typerite.com <br />
-              info@typerite.com <br />
-              Phone: +197 543 2345 </address>
-          </div>
-        </div>
-        <h3>Say Hello</h3>
-        <form id="form" className="form">
-          <fieldset>
-            <div className="form__input-group">
-              <label htmlFor="name">Name:</label>
-              <input type="text" id="name" name="name" required maxLength="16" placeHolder="John Doe" />
-            </div>
-            <div className="form__input-group">
-              <label htmlFor="email">Email:</label>
-              <input type="email" id="email" name="email" required placeHolder="john@example.com" />
-            </div>
-            <div className="form__input-group">
-              <label htmlFor="text">Website:</label>
-              <input type="text" id="text" name="text" required placeHolder="john-doe.co.uk" />
-            </div>
-            <div className="form__input-group">
-              <label htmlFor="msg">Message:</label>
-              <textarea name="msg" className="form__textarea" id="msg" required />
-            </div>
-          </fieldset>
-          <button className="form__button" type="submit" form="form">send Message</button>
-        </form>
-      </main>
-      <Footer />
+          <h3 className="post-h3">Say Hello</h3>
+          <form id="form" className="form">
+            <fieldset className="form__fieldset">
+              <div className="form__input-group">
+                <label htmlFor="name">Name:</label>
+                <input className="form__input" type="text" id="name" name="name" required maxLength="16" placeHolder="John Doe" />
+              </div>
+              <div className="form__input-group">
+                <label htmlFor="email">Email:</label>
+                <input className="form__input" type="email" id="email" name="email" required placeHolder="john@example.com" />
+              </div>
+              <div className="form__input-group">
+                <label htmlFor="text">Website:</label>
+                <input className="form__input" type="text" id="text" name="text" required placeHolder="john-doe.co.uk" />
+              </div>
+              <div className="form__input-group">
+                <label htmlFor="msg">Message:</label>
+                <textarea name="msg" className="form__textarea" id="msg" required />
+              </div>
+            </fieldset>
+            <button className="form__button" type="submit" form="form">send Message</button>
+          </form>
+        </main>
+        <Footer />
+      </div>
     </>
   )
 }
