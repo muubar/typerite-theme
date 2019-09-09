e24e1f6d4c597456b9245ad7d0d4222de40971aa
Ahmed <ahmedmudhar@hotmail.com>
2019-09-09 08:36:52 +0200

    implement route-based lazy loading

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom";
 import { Route, Switch, Redirect, BrowserRouter as Router } from "react-router-dom";
 
-import Home from "./components/pages/homePage";
-import StandardPost from "./components/pages/standardPostPage"
-import About from "./components/pages/aboutPage";
-import Contact from "./components/pages/ContactPage";
+const StandardPost = lazy(() => import('./components/pages/standardPostPage'));
+const Contact = lazy(() => import('./components/pages/contactPage'));
+const Home = lazy(() => import('./components/pages/homePage'));
+const About = lazy(() => import('./components/pages/aboutPage'));
+
+const Loader = <div style={{
+  position: "fixed", width: "300px", height: "60px", fontSize: "36px", fontFamily: "arial",
+  top: "calc( 50% - 30px )", right: "calc( 50% - 200px )"
+}}>
+  Loading...
+  </div>;
 
 function Routing() {
   return (
     < Router >
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/posts/standard-post" component={StandardPost} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        <Redirect to="/" />
-      </Switch>
+      <Suspense fallback={Loader}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/posts/standard-post" component={StandardPost} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Redirect to="/" />
+        </Switch>
+      </Suspense>
     </Router >
   )
 }
